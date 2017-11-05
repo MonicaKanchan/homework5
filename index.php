83661f9a7ee151c7d611de775623304a9d7274fe
monica nagendra kanchan <mk758@afsconnect1.njit.edu>
2017-11-05 14:26:53 -0500

    database connected

--- index.php
+++ index.php
@@ -1,17 +1,36 @@
 <?php
-$servername = "sql1.njit.edu";
-$username = "mk758";
-$password = "MxLEzvEVX";
-$dbname="mk758";
+
+class dbconn
+{
+protected static $conn;
+
+private function __construct()
+{
 try
 {
-$conn = new PDO("mysql:host=$servername;dbname=mk758", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "Connected successfully". "<br>" ;
+self::$conn = new PDO("mysql:host=sql1.njit.edu;dbname=mk758", "mk758", "MxLEzvEVX");
+self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e)
 {
-echo "Connection failed: " . "<br>" . $e->getMessage();
+echo "Connection failed: " ."<br>". $e->getMessage();
 
 }
+}
+
+public static function getConnection()
+{
+if (!self::$conn)
+{
+new dbconn();
+}
+
+return self::$conn;
+}
+}
+
+$conn = dbconn::getConnection();
+echo "<b>Connected Successfully</b>"."<br>";
+
+
 ?>
